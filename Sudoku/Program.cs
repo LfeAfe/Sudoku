721b20c475860ccd10e282908ab726fad78cae52
ProjectFede <pastori.federicoit@gmail.com>
2026-02-05 15:09:01 +0100

    INIZIALIZZAZIONE MENU STAR
    Creazione dei comandi start ed esci

--- Sudoku/Program.cs
+++ Sudoku/Program.cs
@@ -11,6 +11,30 @@ namespace Sudoku
         static void Main(string[] args)
         {
             // menu start
+        
+            Console.WriteLine("Welcome to Sudoku!");
+            Console.WriteLine("1 - inizia");
+            Console.WriteLine("2 - esci");
+            Console.Write("Scegli un'opzione: ");
+            string scelta = Console.ReadLine();
+
+
+            if (scelta == "1")
+            {
+                Console.WriteLine("Inizio del gioco...");
+            }
+            else if (scelta == "2")
+            {
+                // Exit the application
+                Console.WriteLine("Uscita dall'applicazione. Arrivederci!");
+            }
+            else
+            {
+                Console.WriteLine("Opzione non valida. Riprova.");
+            }
+
+
+
         }
     }
 }
