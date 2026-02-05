f91740281539f5e86a772b034d8ea35175bc9a25
Costa Francesco <francesco.costa@itsincom.it>
2026-02-05 15:00:04 +0100

    aggiunta matrice di base
    creazione della matrice che sta a base del sudoku

---
+++ Sudoku/Classi/Cella.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sudoku.Classi
+{
+    public class Cella
+    {
+        public int? contenuto { get; set; }
+        public bool modificabile { get; private set; }
+
+        public Cella(int? contenuto, bool modificabile)
+        {
+            this.contenuto = contenuto;
+            this.modificabile = modificabile;
+        }
+    }
+}
--- Sudoku/Program.cs
+++ Sudoku/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Sudoku.Classi;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,17 @@ namespace Sudoku
     {
         static void Main(string[] args)
         {
+            Cella[,] sudoku = new Cella[9,9];
+
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    sudoku[i, j] = new Cella(0, true);
+                    Console.Write(sudoku[i, j].contenuto + " ");
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
