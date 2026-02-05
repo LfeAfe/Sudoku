a3e14af61db35150bc7a959531943cf1d6698521
ProjectFede <pastori.federicoit@gmail.com>
2026-02-05 16:16:46 +0100

    AGGIUNTA E SISTEMAZIONE DEI COMMENTI NEL CODICE

--- Sudoku/Program.cs
+++ Sudoku/Program.cs
@@ -10,43 +10,43 @@ namespace Sudoku
     {
         static void Main(string[] args)
         {
-            // menu start
+            // menu start: visualizzazione scelte
 
-            Console.WriteLine("Welcome to Sudoku!");
-            Console.WriteLine("1 - inizia");
+            Console.WriteLine("Welcome to Sudoku!"); 
+            Console.WriteLine("1 - inizia"); 
             Console.WriteLine("2 - esci");
             Console.Write("Scegli un'opzione: ");
-            string scelta = Console.ReadLine();
+            string scelta = Console.ReadLine(); //lettura della scelta dell'utente
 
 
-            if (scelta == "1")
+            if (scelta == "1") //se la scelta dell'utente è 1 farà...
             {
-                Console.WriteLine("Inizio del gioco...");
+                Console.WriteLine("Inizio del gioco..."); //inizio
             }
-            else if (scelta == "2")
+            else if (scelta == "2") //se la scelta dell'utente è 2 farà...
             {
-                // Exit the application
-                Console.WriteLine("Uscita dall'applicazione. Arrivederci!");
+                Console.WriteLine("Uscita dall'applicazione. Arrivederci!"); //uscita dal codice
             }
-            else
+            else // se l'utente inserisce un valore diverso da 1 o 2 farà...
             {
-                Console.WriteLine("Opzione non valida. Riprova.");
+                Console.WriteLine("Opzione non valida. Riprova."); //messaggio di errore
             }
 
+            // definizione delle tre variabili riga, colonna e numero
             int riga;
             int colonna;
             int numero;
 
-
+            //cicli per la richiesta dei tre valori
             while (true) 
             {
-                Console.Write("Inserisci la riga (1-9): ");
-                if (int.TryParse(Console.ReadLine(), out riga) && riga >= 1 && riga <= 9)
+                Console.Write("Inserisci la riga (1-9): "); //richiesta della riga
+                if (int.TryParse(Console.ReadLine(), out riga) && riga >= 1 && riga <= 9) //definizione che il valore dovrà essere tra 1 e 9
                     break;
-                Console.WriteLine("valore non valido");
+                Console.WriteLine("valore non valido"); //errore se il numero non è 1 o 9
                
             }
-
+            //i seguenti cicli fanno le stesse cose ma per colonna e numero
             while (true)
             {
                 Console.Write("inserisci la colonna (1-9): ");
@@ -63,12 +63,12 @@ namespace Sudoku
                 Console.WriteLine("valore non valido");
             }
             
-        
-        
+     
         }
 
-
+       
 
         }
+    
     }
 
